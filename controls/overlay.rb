af29f93a023dd35ab014c3f0289ccc0fc5c884ab
George M. Dias <gdias@mitre.org>
2025-05-19 12:51:12 -0500

    Update overlay.rb
    Updated object_public_privileges for v-233520

--- controls/overlay.rb
+++ controls/overlay.rb
@@ -93,7 +93,8 @@ include_controls 'crunchy-data-postgresql-stig-baseline' do
 
       #object_granted_privileges = 'arwdDxtU?'
       object_granted_privileges = 'arwd(DxtU?|)?'
-      object_public_privileges = 'r'
+      #object_public_privileges = 'r'
+      object_public_privileges = '(r|arw|rw|arwd|rU)'
       object_acl = "^((((#{owners})=[#{object_granted_privileges}]+|"\
         "=[#{object_public_privileges}]+)\/\\w+,?)+|)\\|"
       object_acl_regex = Regexp.new(object_acl)
